1149f110d101e6372d18ad52c020f11fb3e61697
Matt Larner <matt.larner.dev@gmail.com>
2016-02-05 09:04:03 +1000

    one test

--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,10 @@
-var tape = require('tape');
+var test = require('tape'),
+    bar = require('../');
+
+test('morrison exists', function(t) {
+    t.plan(2);
+
+    var testBar = bar;
+    t.ok(testBar, 'morrison exists');
+    t.equal(typeof testBar, 'function', 'morrison is a function');
+});
